73605307820f7ec847bb83a38713704f86532941
darlene-21 <darlenepeloso@gmail.com>
2025-11-27 20:59:18 -0300

    Add files via upload

---
+++ JS/Java.js
@@ -0,0 +1,12 @@
+console.log ("El script cargo");
+
+
+function Feliz() {
+    document.body.style.setProperty("background-image", "url('./Imagenes/feliz.jpg')", "important");
+    document.body.style.setProperty("background-size", "cover", "important");
+    document.body.style.setProperty("background-repeat", "no-repeat", "important");
+
+    document.documentElement.style.setProperty("background-image", "url('./Imagenes/feliz.jpg')", "important");
+    document.documentElement.style.setProperty("background-size", "cover", "important");
+    document.documentElement.style.setProperty("background-repeat", "no-repeat", "important");
+}
