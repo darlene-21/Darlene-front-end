c0cd66ebb419e9b7b8c52c1688138cad7c7335f7
darlene-21 <darlenepeloso@gmail.com>
2025-12-04 20:53:00 -0300

    Add files via upload

--- JS/Java.js
+++ JS/Java.js
@@ -1,21 +1,6 @@
 console.log ("El script cargo");
 
 
-function Feliz() {
-    document.body.style.setProperty("background-image", "url('./Imagenes/feliz.jpg')", "important");
-    document.body.style.setProperty("background-size", "cover", "important");
-    document.body.style.setProperty("background-repeat", "no-repeat", "important");
-
-    document.documentElement.style.setProperty("background-image", "url('./Imagenes/feliz.jpg')", "important");
-    document.documentElement.style.setProperty("background-size", "cover", "important");
-    document.documentElement.style.setProperty("background-repeat", "no-repeat", "important");
-}
-function Mal() {
-    document.body.style.setProperty("background-image", "url('./Imagenes/mal.jpg')", "important");
-    document.body.style.setProperty("background-size", "cover", "important");
-    document.body.style.setProperty("background-repeat", "no-repeat", "important");
-
-    document.documentElement.style.setProperty("background-image", "url('./Imagenes/mal.jpg')", "important");
-    document.documentElement.style.setProperty("background-size", "cover", "important");
-    document.documentElement.style.setProperty("background-repeat", "no-repeat", "important");
-}
+document.getElementById('checkNativeSwitch').addEventListener('change', function() {
+  document.body.classList.toggle('dark-mode', this.checked);
+});
